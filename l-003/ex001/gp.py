dd3455cb463b41e27dc3c338091d99b8638654b6
Luiz Felipe dos Santos <felipe.pinghang2@gmail.com>
2023-02-28 17:56:42 -0300

    lista 003 ex001

---
+++ l-003/ex001/gp.py
@@ -0,0 +1,13 @@
+print("\n-=- Exercício 001 -=-")
+print("Faça um programa que peça uma nota, entre zero e dez. Mostre uma mensagem caso o valor seja inválido e continue pedindo até que o usuário informe um valor válido.\n")
+
+#Programa:
+
+n1=-1
+while n1>10 or n1<0:
+    n1=int(input("Coloque uma nota:"))
+print("Nota Aceita.")
+
+#Fim do Programa.
+
+print("\n--- Fim do Programa ---\n")
